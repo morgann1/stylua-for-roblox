431af12a76f96de2351979332816985535f1c5ca
morgann1 <58310810+morgann1@users.noreply.github.com>
2026-04-21 19:06:17 +0100

    init commit

---
+++ plugin/Packages/SharedToolbar.lua
@@ -0,0 +1,5 @@
+local REQUIRED_MODULE = require(script.Parent._Index["morgann1_shared-toolbar@0.2.2"]["shared-toolbar"])
+export type ButtonSpec = REQUIRED_MODULE.ButtonSpec 
+export type RegisterOptions = REQUIRED_MODULE.RegisterOptions 
+export type SharedToolbarHandle = REQUIRED_MODULE.SharedToolbarHandle 
+return REQUIRED_MODULE
